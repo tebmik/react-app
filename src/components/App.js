14acd0b3aa1b2c06f33f66f8c9ab88b58b074d17
TebMik <lloyd.s.tom@gmail.com>
2019-12-04 12:44:28 +0000

    App and Header commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
     return(
         <div>
             <BrowserRouter>
+                <Header />
                 <div>
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
--- src/components/README.md
+++ src/components/README.md
@@ -1,1 +1,12 @@
-in App.js, imported all stream components and set them on individual paths using "Route"
+Created a new component named Header 
+
+imported Link from react-router-dom
+
+Styled it using Semantic-ui.
+
+nested Header component inside BrowserRouter in App.js so it can be seen for each url change
+
+eg. 
+    /streams/show
+    /streams/new
+    /streams/edit etc.
