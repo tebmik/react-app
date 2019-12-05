2973ad350fb601d7f2724252d26bcf1f4fd02a56
TebMik <lloyd.s.tom@gmail.com>
2019-12-05 14:06:38 +0000

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS.EXTENSION.COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducers,
